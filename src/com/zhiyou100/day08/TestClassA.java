32f25d7656bb66c97638705519b4e7df1bc7c01c
1123Javayanglei <yanglei1123@163.com>
2020-01-14 08:49:14 +0800

    增加了sout*4

---
+++ src/com/zhiyou100/day08/TestClassA.java
@@ -0,0 +1,25 @@
+package com.zhiyou100.day08;
+
+/**
+ * @author yanglei
+ */
+class ClasssA {
+    int value;
+
+    public void method(int value) {
+        System.out.println(value);
+    }
+
+    public void method() {
+        System.out.println(value);
+    }
+}
+
+class TestClassA {
+    public static void main(String[] args) {
+        ClasssA classA = new ClasssA();
+        classA.value = 10;
+        classA.method();
+        classA.method(20);
+    }
+}
