66f706b2d294f8b70b1b5b3c34d1387e77051f3b
1123Javayanglei <yanglei1123@163.com>
2020-02-03 17:01:31 +0800

    复习

--- src/com/zhiyou100/review/OOPReview.java
+++ src/com/zhiyou100/review/OOPReview.java
@@ -5,6 +5,32 @@ package com.zhiyou100.review;
  */
 public class OOPReview {
     public static void main(String[] args) {
-        
+        Person person=new Person();
+        person.age=18;
+        person.name="杨磊";
+        person.job="coder";
+        person.sayHi();
+        person.add(1,2.0);
     }
+}
+class Person{
+    int age;
+    String name;
+    String job;
+    void sayHi(){
+        System.out.println("我的名字是"+name+"\t工作是"+job+"\t今年"+age+"岁");
+    }
+    /**
+     * 方法重载：方法名相同，参数列表不同
+     */
+    int add(int a,int b){
+        return a+b;
+    }
+    double add(int a,double b){
+        return a+b;
+    }
+    int add(int a){
+        return a+1;
+    }
+
 }
