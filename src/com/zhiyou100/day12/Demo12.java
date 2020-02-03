04824222775d10cf40ad1ef9ccaccfeba72e31e4
1123Javayanglei <yanglei1123@163.com>
2020-02-03 16:00:12 +0800

    复习第一天

--- src/com/zhiyou100/day12/Demo12.java
+++ src/com/zhiyou100/day12/Demo12.java
@@ -4,5 +4,14 @@ package com.zhiyou100.day12;
  * @author yanglei
  */
 public class Demo12 {
-    
+    public static void main(String[] args) {
+        double a=3.0;
+        double b=1.0+3.0;
+        if (a==b){
+            System.out.println("ok");
+        }else {
+            System.out.println("No");
+        }
+        System.out.println(" a="+a+" b="+b);
+    }
 }
