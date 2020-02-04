2637761128d97c17e688599f4cc003a1403ac1d8
1123Javayanglei <yanglei1123@163.com>
2020-02-04 16:02:42 +0800

    内存图

--- src/com/zhiyou100/review/OOPReview.java
+++ src/com/zhiyou100/review/OOPReview.java
@@ -6,17 +6,43 @@ package com.zhiyou100.review;
 public class OOPReview {
     public static void main(String[] args) {
         Person person=new Person();
+        Person person2=new Person(1);
         person.age=18;
         person.name="杨磊";
         person.job="coder";
         person.sayHi();
         person.add(1,2.0);
+        System.out.println("\n");
+
+        Demo1 demo1=new Demo1();
+        System.out.println(demo1);
+        demo1.show();
     }
 }
 class Person{
     int age;
     String name;
     String job;
+
+    /**
+     * 成员变量 定义在类中的变量:
+     * age,name,job
+     */
+    Person(){
+        System.out.println("无参数的构造方法1");
+        return;
+    }
+    Person (int a){
+        // 构造方法互相调用用this(参数列表)
+        this();
+    }
+    void test(int a){
+        /*
+         * 局部变量 定义在方法中的变量
+         * a
+         */
+
+    }
     void sayHi(){
         System.out.println("我的名字是"+name+"\t工作是"+job+"\t今年"+age+"岁");
     }
@@ -33,4 +59,9 @@ class Person{
         return a+1;
     }
 
+}
+class Demo1{
+    void show(){
+        System.out.println(this);
+    }
 }
---
+++ src/com/zhiyou100/review/Review02.java
@@ -0,0 +1,29 @@
+package com.zhiyou100.review;
+
+/**
+ * @author yanglei
+ */
+public class Review02 {
+    public static void main(String[] args) {
+        Car c1=new Car('白',"吉利");
+        c1.color='红';
+        c1.show();
+        Car c2=c1;
+        c2.setMoney(2000);
+    }
+}
+class Car{
+    char color;
+    double money=1000.0;
+    String logo="bmw";
+    void show(){
+        System.out.println(color+","+logo+","+money);
+    }
+    public Car(char color,String logo){
+        this.color=color;
+        this.logo=logo;
+    }
+    void  setMoney(double money){
+        this.money=money;
+    }
+}
