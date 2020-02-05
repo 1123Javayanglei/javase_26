c1daa75953bb88d658a5b2076ac799b06dc09c0a
1123Javayanglei <yanglei1123@163.com>
2020-02-05 15:59:32 +0800

    day03
    给属性赋值的方法

---
+++ src/com/zhiyou100/review/oop/day02/OPPFinal.java
@@ -0,0 +1,40 @@
+package com.zhiyou100.review.oop.day02;
+
+/**
+ * @author yanglei
+ */
+public class OPPFinal {
+    public static void main(String[] args) {
+        Demo00 demo00 = new Demo00();
+        demo00.printPI();
+        demo00.printName();
+    }
+}
+
+class Demo00 {
+    int a;
+    //    final int b;
+    final int b = 11;
+    /*
+     *  Variable 'b' might not have been initialized final
+     * final类型的变量没有默认初始值，必须显示赋值
+     * final类型的变量只能赋值一次
+     */
+//    b=12;
+    final double PI = Math.PI;
+
+    final String name = "哔哩哔哩🍻";
+
+    void printPI() {
+        System.out.println(PI);
+    }
+
+    void printName() {
+        System.out.println(name);
+    }
+
+    void test00(final int b) {
+        System.out.println(b);
+    }
+
+}
