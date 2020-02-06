f2c8177dcff8fdea5b4264f9a6ae9453d14e06e7
1123Javayanglei <yanglei1123@163.com>
2020-02-06 22:27:51 +0800

    Update Main.java

--- src/com/zhiyou100/review/oop/day04/test/Main.java
+++ src/com/zhiyou100/review/oop/day04/test/Main.java
@@ -26,7 +26,7 @@ class Student extends Person {
     protected int score;
 
     public   Student(String name, int age, int score) {
-//        super(name,age);
+        super(name,age);
         this.score = score;
     }
 }
