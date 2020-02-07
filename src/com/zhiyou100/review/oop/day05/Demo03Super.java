707688b116fc148ce2005e86a6b8b5ac6b827e76
1123Javayanglei <yanglei1123@163.com>
2020-02-07 17:16:43 +0800

    工资系统
    封装、继承、多态全部使用

---
+++ src/com/zhiyou100/review/oop/day05/Demo03Super.java
@@ -0,0 +1,37 @@
+package com.zhiyou100.review.oop.day05;
+
+import java.rmi.Naming;
+
+/**
+ * @author yanglei
+ * @date 2020/2/7 3:35 下午
+ */
+public class Demo03Super {
+    public static void main(String[] args) {
+        Son01 son01 = new Son01();
+        // 创建子类对象时 会调用父类的无参数的构造方法
+        son01.show();
+    }
+}
+class Father01{
+    int a=1;
+    int b=2;
+    Father01(int a){
+        this.a=a;
+    }
+    Father01(){
+        System.out.println("无参数的构造方法");
+    }
+}
+class Son01 extends Father01{
+
+    // There is no default constructor available in 'com.zhiyou100.review.oop.day05.Father01'
+    void show(){
+
+        System.out.println(a+"::"+b);
+    }
+    Son01(){
+        super();
+        // 所有的子类构造方法，第一个语句都是 super() 来调用父类的无参数的构造方法
+    }
+}
