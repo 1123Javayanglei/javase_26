1b86a47666846b47da79c190f30cec728f5cf699
1123Javayanglei <yanglei1123@163.com>
2020-02-18 19:57:52 +0800

    练习

--- src/com/zhiyou100/review/oop/day10/Demo05OfInstanceof.java
+++ src/com/zhiyou100/review/oop/day10/Demo05OfInstanceof.java
@@ -11,8 +11,10 @@ public class Demo05OfInstanceof {
     public static void main(String[] args) {
         showAnimal(new Cat());
         showAnimal(new Dog());
+        showAnimal(new Animal());
     }
-    public static void showAnimal(Animal animal){
+
+    public static void showAnimal(Animal animal) {
         // 只能调用实现类在接口中定义过的方法
 //         animal.eat();
 //         animal.show();
@@ -22,8 +24,9 @@ public class Demo05OfInstanceof {
          * 先判断多态对象的具体类型： 展现得是Animal
          * 判断对态对象的具体类型 通过 instanceof 关键字 是运算符
          */
-        System.out.println("animal 是 Cat 类型的吗:"+(animal instanceof Cat));
-        System.out.println("animal 是 Dog 类型的吗:"+(animal instanceof Dog));
+        System.out.println("animal 是 Cat 类型的吗:" + (animal instanceof Cat));
+        System.out.println("animal 是 Dog 类型的吗:" + (animal instanceof Dog));
+        System.out.println("animal 是 Animal 类型的吗:" + (animal instanceof Animal));
         /**
          * 把子类对象伪装成发了对象 ：： 向上转型
          *  格式 ： 父类引用 = 子类对象;
@@ -40,28 +43,30 @@ public class Demo05OfInstanceof {
 //        Dog dog =(Dog)animal;
         //将 'animal' 转换为 'Dog' 可能产生 'ClassCastException'
         if (animal instanceof Cat) {
-            Cat cat=(Cat)animal;
+            Cat cat = (Cat) animal;
             // 向下转型
             cat.sport();
             // 调用cat特有的成员
-        }else if (animal instanceof Dog){
-            Dog dog =(Dog)animal;
+        } else if (animal instanceof Dog) {
+            Dog dog = (Dog) animal;
             // 向下转型
             // 调用dog的特有成员
             dog.sport();
         }
 
 
-
-
     }
 }
-interface Animal {
-    void eat();
-    void show();
+
+class Animal {
+    void eat(){};
+
+    void show(){};
 }
-class Dog implements Animal {
+
+class Dog extends Animal {
     String name;
+
     @Override
     public void eat() {
         System.out.println("狗吃骨头");
@@ -69,13 +74,17 @@ class Dog implements Animal {
 
     @Override
     public void show() {
-        System.out.println("狗 名字"+name);
+        System.out.println("狗 名字" + name);
     }
-    public void sport(){
-        System.out.println("狗 名字"+name+",在抓老鼠!!!");
+
+    public void sport() {
+        System.out.println("狗 名字" + name + ",在抓老鼠!!!");
     }
-}class Cat implements Animal {
+}
+
+class Cat extends Animal {
     String name;
+
     @Override
     public void eat() {
         System.out.println("猫吃鱼");
@@ -83,9 +92,10 @@ class Dog implements Animal {
 
     @Override
     public void show() {
-        System.out.println("猫 名字"+name);
+        System.out.println("猫 名字" + name);
     }
-    public void sport(){
-        System.out.println("猫 名字"+name+",在晒太阳!!!");
+
+    public void sport() {
+        System.out.println("猫 名字" + name + ",在晒太阳!!!");
     }
 }
