7f9c818100d8a3bc0dc9a7e2ed6599114652e0a0
1123Javayanglei <yanglei1123@163.com>
2020-02-22 12:25:39 +0800

    String 类

--- src/com/zhiyou100/basicclass/day02/HomeWorkMath.java
+++ src/com/zhiyou100/basicclass/day02/HomeWorkMath.java
@@ -3,7 +3,7 @@ package com.zhiyou100.basicclass.day02;
 import java.util.Scanner;
 
 /**
- * @packageName: javase_26
+ * @packageName: javaee_26
  * @className: HomeWorkMath
  * @Description: TODO
  * @author: YangLei
@@ -12,12 +12,17 @@ import java.util.Scanner;
 public class HomeWorkMath {
     public static void main(String[] args) {
 
+        for (int i = 0; i < 100; i++) {
+//            System.out.println(HomeWorkOfMath.randomDate());
+            System.out.println(HomeWorkOfMath.createNewName());
+        }
+
     }
 }
 
 class HomeWorkOfMath {
     public static double myRounding(double in) {
-        /**
+        /*
          * @name: rounding
          * @param: double in
          * @description: TODO 四舍六入，五取偶
@@ -28,12 +33,14 @@ class HomeWorkOfMath {
         // flagOfTenths 获取十分上的数
         int flagOfSingles = (int) (in % 10);
         // 获取个数上的数
-        if (flagOfTenths < 5) {
+        int max=5;
+        int evenNumber=2;
+        if (flagOfTenths < max) {
             //四舍
             return (double) ((int) (in));
-        } else if (flagOfTenths == 5) {
+        } else if (flagOfTenths == max) {
             // 5取偶
-            if (flagOfSingles % 2 == 0) {
+            if (flagOfSingles % evenNumber == 0) {
                 //偶数不变
                 return (double) (((int) (in)));
             } else {
@@ -48,7 +55,7 @@ class HomeWorkOfMath {
     }
 
     public static boolean determineIfTheInputNumberIsPrime() {
-        /**
+        /*
          * @name: determineIfTheInputNumberIsPrime
          * @param: void
          * @description: TODO 判断键盘输入的数是不是质数
@@ -67,7 +74,7 @@ class HomeWorkOfMath {
                 break;
             }
         }
-        if (primeOfFlag || number == 1) {
+        if (primeOfFlag || number == 1||number<1) {
             return false;
         } else {
             return true;
@@ -75,7 +82,7 @@ class HomeWorkOfMath {
     }
 
     public static String randomDate() {
-        /**
+        /*
          * @name: randomDate
          * @param:
          * @description: TODO 随机一个日期，格式 XXXX-XX-XX ,XX:XX:XX,范围1900-1-1 00:00:00 ~ 2020-12-31 23:59:60
@@ -102,45 +109,45 @@ class HomeWorkOfMath {
     }
 
     public static String addZero(int num) {
-        /**
+        /*
          * @name: addZero
          * @param: int num
          * @description: TODO  把 1 变成 01
          * @date: 2020/2/21 5:43 下午
          * @return: String
          */
-
-        if (num / 10 == 0) {
+        int theUnit=10;
+        if (num / theUnit == 0) {
             // 1/10 =  0，是个位
             return "0" + num;
         } else {
             return String.valueOf(num);
-
+            // Int -> String
         }
     }
 
     public static int randomOfMinToMax(double min, double max) {
-        /**
+        /*
          * @name: randomOfMinToMax
          * @param: double min,double max
          * @description: TODO  生成 [min,max]的随机数
          * @date: 2020/2/21 5:17 下午
-         * @return:
+         * @return: int
          */
         return (int) (min + (int) (Math.random() * (max - min + 1)));
     }
 
     public static int judgeTheDaysOfTheMonth(int year, int month) {
-        /**
+        /*
          * @name: judgeTheDaysOfTheMonth
          * @param: int year ,int month
          * @description: TODO  判断本月有多少天
          * @date: 2020/2/21 5:00 下午
-         * @return:
+         * @return: int
          */
         boolean yearLag = (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
         // yearLag true为闰年，false为平年
-        /**
+        /*
          * 平年的2月份是28天
          * 闰年的2月份是29天
          */
@@ -169,7 +176,7 @@ class HomeWorkOfMath {
     }
 
     public static String createNewName() {
-        /**
+        /*
          * @name: createNewName
          * @param:
          * @description: TODO 随机生成一个由小写字母组成的名字,字母个数[4,8]
@@ -177,22 +184,23 @@ class HomeWorkOfMath {
          * @return:
          */
         // a=97,z=122;
-        String name = getRandomCharToString()+getRandomCharToString()+getRandomCharToString()+getRandomCharToString()+getRandomCharToString()+getRandomCharToString()+getRandomCharToString()+getRandomCharToString();
+        String name = getRandomCharToString() + getRandomCharToString() + getRandomCharToString() + getRandomCharToString() + getRandomCharToString() + getRandomCharToString() + getRandomCharToString() + getRandomCharToString();
         // 拼接
         int randomNum = randomOfMinToMax(4, 8);
         return name.substring(0, randomNum);
         // 切片
 
     }
-    public static String getRandomCharToString(){
-        /**
+
+    public static String getRandomCharToString() {
+        /*
          * @name: getRandomCharToString
          * @param:
          * @description: TODO  随机获取一个小写字母，转换为字符串
          * @date: 2020/2/21 6:27 下午
          * @return:
-        */
-        return String.valueOf((char) (randomOfMinToMax(97,122)));
+         */
+        return String.valueOf((char) (randomOfMinToMax(97, 122)));
     }
 }
 
---
+++ src/com/zhiyou100/basicclass/day03/Demo02OfString.java
@@ -0,0 +1,62 @@
+package com.zhiyou100.basicclass.day03;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @packageName: javase_26
+ * @className: Demo02OfString
+ * @Description: TODO
+ * @author: YangLei
+ * @date: 2020/2/22 11:31 上午
+ */
+public class Demo02OfString {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String s="123abcd";
+        String ss=new String("123Abcd");
+        /**
+         * 字符串方法
+         *  1、判断相关的方法
+         *       boolean contains(String s)  判断参数字符串是不是当前字符串的子串
+         *       boolean equals（String s) 判断参数字符串和当前字符串，序列是否相同
+         *       boolean equalsIgnoreCase(String s) 不区分大小写
+         *       boolean endWith(String s) 判断当前字符串对象是否是参数字符串结尾
+         *       boolean StartWith(String s) 判断当前字符串对象是否是参数字符串开头
+         *       boolean isEmpty() 判断当前字符串是否为空，字符长度为0
+         *
+         *  2、获取相关的方法
+         *      int length() 获取当前字符串的字符个数
+         *      char charAt(int index) 获取参数索引处对应的字符
+         *      byte getBytes() 使用平台的默认字符集将此 String 编码为 byte 序列，并将结果存储到一个新的 byte 数组中。
+         *      byte getBytes(Charset charset) 使用给定的 charset 将此 String 编码到 byte 序列，并将结果存储到新的 byte 数组。
+         *      char[]	toCharArray() 将此字符串转换为一个新的字符数组。
+         *
+         */
+
+        byte[] arrBytes = "adb杨磊".getBytes("GBK");
+        // Unhandled exception: java.io.UnsupportedEncodingException 解决方法，抛出异常
+        for (byte arrByte : arrBytes) {
+            System.out.println(arrByte);
+            // gbk 一个汉字对应两个字节
+            // utf-8 一个汉字对应三个字节
+        }
+        char[] arrBytes01 = "sdfa".toCharArray();
+        for (int i = 0; i < arrBytes01.length; i++) {
+            System.out.println(arrBytes01[i]);
+        }
+        System.out.println();
+        System.out.println("234324".length());
+        System.out.println(s.contains("12"));
+        // true
+        System.out.println(s.contains("1a"));
+        // false
+
+        System.out.println(s.equals(ss));
+        System.out.println(s.equalsIgnoreCase(ss));
+
+        System.out.println(s.endsWith("cd"));
+        System.out.println(s.startsWith("1"));
+
+        System.out.println(s.isEmpty());
+
+    }
+}
